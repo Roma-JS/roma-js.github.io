18f120df5c94d5549841da1a06db8d72928e842d
gdorsi <guido@float.com>
2023-09-30 12:38:27 +0000

    Deploying to master from @ Roma-JS/roma-js-on-astro@f3617dddb4b8620428bbb57af2177ee9d60977fd 🚀

---
+++ _astro/hoisted.2cf18a7d.js
@@ -0,0 +1,1 @@
+import"./ViewTransitions.astro_astro_type_script_index_0_lang.a23147bd.js";
---
+++ _astro/langSelector.module.1122c135.92c20e2c.js
@@ -0,0 +1,1 @@
+const t="_nav_zw2cg_1 d-contents",n="_list_zw2cg_5 d-flex m-0",s="_listItem_zw2cg_12 d-flex m-0",l="_langBtn_zw2cg_30 d-flex",_={nav:t,list:n,listItem:s,langBtn:l};export{_ as s};
